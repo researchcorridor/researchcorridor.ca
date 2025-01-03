2d2779e576923ce90a76e89691317dad0a3857e9
M Anas Latif <m.anaslatif.com@gmail.com>
2025-01-04 03:46:11 +0500

    theme setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,36 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import { nextui } from '@nextui-org/react';
 
 export default {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-jacques-francois)', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        primary: {
+          DEFAULT: '#FF9595',
+          foreground: '#ffffff',
+          50:  '#fff2f2',
+          100: '#ffe4e4',
+          200: '#ffd7d7',
+          300: '#ffc9c9',
+          400: '#ffbbbb',
+          500: '#ffadad',
+          600: '#ff9e9e',
+          700: '#f99292',
+          800: '#ec8a8a',
+          900: '#de8282',
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 } satisfies Config;
