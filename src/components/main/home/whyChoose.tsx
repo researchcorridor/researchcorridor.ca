b66c35f09f54c4d6a07d3e5960ccb99860188441
M Anas Latif <m.anaslatif.com@gmail.com>
2025-01-05 22:36:45 +0500

    design whyChoose section

--- src/components/main/home/whyChoose.tsx
+++ src/components/main/home/whyChoose.tsx
@@ -1,7 +1,31 @@
 'use client';
+import homeText from '@/constant/homePage';
 
 const WhyChoose = () => {
-  return <section id="why-choose">why choose</section>;
+  return (
+    <section id="why-choose" className="py-20">
+      <div className="mx-auto max-w-7xl p-6 max-[500px]:p-1">
+        <h2 className="text-center text-4xl max-md:text-3xl max-sm:text-2xl">
+          {homeText.whyChoose.title}
+        </h2>
+        <div className="mt-20 grid grid-cols-4 gap-5 max-md:grid-cols-2 max-sm:mt-10 max-[500px]:gap-y-10">
+          {homeText.whyChoose.cards.map((item, index) => (
+            <div key={index} className="flex flex-col gap-3 max-[500px]:gap-2">
+              <h3 className="flex justify-center text-center text-6xl max-[1000px]:text-5xl">
+                <item.icon />
+              </h3>
+              <h4 className="text-center text-xl max-[1000px]:text-lg max-[400px]:text-base">
+                {item.title}
+              </h4>
+              <p className="text-foreground-600 text-center text-base  max-[1000px]:text-sm">
+                {item.description}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default WhyChoose;
