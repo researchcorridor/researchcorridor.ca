38d6abf5b43d5e3f850434e72cef2cca51cb369c
M Anas Latif <m.anaslatif.com@gmail.com>
2025-01-05 07:19:44 +0500

    design email section

--- src/constant/homePage.ts
+++ src/constant/homePage.ts
@@ -49,6 +49,10 @@ const home = {
   },
   email: {
     title: 'Stay Updated with Latest Research',
+    description:
+      'Subscribe to our newsletter and get the latest updates on research publications, events, and opportunities.',
+    inputText: 'Enter Your Email',
+    buttonText: 'Subscribe',
   },
 };
 
