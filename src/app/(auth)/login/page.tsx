280573e5623acdea26f1f996f020abfdab52b927
M Anas Latif <m.anaslatif.com@gmail.com>
2025-01-25 23:22:05 +0500

    remove cn

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, cn, Form, Input } from '@nextui-org/react';
+import { Button, Form, Input } from '@nextui-org/react';
 import Link from 'next/link';
 import { useRouter } from 'nextjs-toploader/app';
 import { useState } from 'react';
