0c8d7b9b3651ecb82c0a57b117466fe2c53885fb
M Anas Latif <m.anaslatif.com@gmail.com>
2025-03-11 19:39:23 +0500

    check auth on in GET required

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,19 +5,24 @@ import { NextResponse } from 'next/server';
 import { createClient } from './utils/supabase/server';
 
 export const middleware = async (req: any) => {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user) {
-    const url = req.nextUrl.clone();
-    url.pathname = '/login';
-    return NextResponse.redirect(url);
+  const url = req.nextUrl.clone();
+  if (req.method === 'GET') {
+    const supabase = await createClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) {
+      url.pathname = '/login';
+      return NextResponse.redirect(url);
+    }
+    if (req.nextUrl.pathname === '/login') {
+      url.pathname = '/dashboard';
+      return NextResponse.redirect(url);
+    }
   }
-
   return NextResponse.next();
 };
 
 export const config = {
-  matcher: ['/dashboard/:path*'],
+  matcher: ['/dashboard/:path*', '/login/:path*'],
 };
