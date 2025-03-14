25dea1522685a6eb3e30d555c673a41b65e44c91
M Anas Latif <m.anaslatif.com@gmail.com>
2025-03-15 02:33:48 +0500

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,19 +6,19 @@ import { createClient } from './utils/supabase/server';
 
 export const middleware = async (req: any) => {
   const url = req.nextUrl.clone();
-  if (req.method === 'GET') {
-    const supabase = await createClient();
-    const {
-      data: { user },
-    } = await supabase.auth.getUser();
-    if (!user) {
-      url.pathname = '/login';
-      return NextResponse.redirect(url);
-    }
-    if (req.nextUrl.pathname === '/login') {
-      url.pathname = '/dashboard';
-      return NextResponse.redirect(url);
-    }
+  if (req.method !== 'GET') return NextResponse.next();
+  if (url.pathname === '/login') return NextResponse.next();
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) {
+    url.pathname = '/login';
+    return NextResponse.redirect(url);
+  }
+  if (req.nextUrl.pathname === '/login') {
+    url.pathname = '/dashboard';
+    return NextResponse.redirect(url);
   }
   return NextResponse.next();
 };
